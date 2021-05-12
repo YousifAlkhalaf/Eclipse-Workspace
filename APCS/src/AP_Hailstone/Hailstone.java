085d31aa469c02fd870a4b01bfb99bdc537afc58
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2021-05-11 22:07:58 -0700

    hail

--- APCS/src/AP_Hailstone/Hailstone.java
+++ APCS/src/AP_Hailstone/Hailstone.java
@@ -2,4 +2,33 @@ package AP_Hailstone;
 
 public class Hailstone {
 
+	public static int hailstoneLength (int n) {
+		int length = 1;
+		while (n > 1) {
+			if (n % 2 == 0) {
+				n /= 2;
+			} else {
+				n = n * 3 + 1;
+			}
+			length++;
+		}
+		return length;
+	}
+	
+	public static boolean isLongSeq (int n) {
+		if (hailstoneLength(n) > n) {
+			return true;
+		}
+		return false;
+	}
+	
+	public static double propLong (int n) {
+		int longSeqs = 0;
+		for (int i = 1; i <= n; i++) {
+			if (isLongSeq(i)) {
+				longSeqs++;
+			}
+		}
+		return (double) longSeqs/ (double) n;
+	}
 }
--- APCS/src/AP_Hailstone/HailstoneRunner.java
+++ APCS/src/AP_Hailstone/HailstoneRunner.java
@@ -2,4 +2,32 @@ package AP_Hailstone;
 
 public class HailstoneRunner {
 
+	public static void main(String[] args) {
+
+		int[] values = { 5, 8, 1, 14 };
+
+		for (int value : values) {
+			int hs = Hailstone.hailstoneLength(value);
+			System.out.printf("The length starting at %d is %d\n", value, hs);
+		}
+
+		System.out.println();
+
+		for (int value : values) {
+			if (Hailstone.isLongSeq(value))
+				System.out.printf("The length starting at %d is long\n", value);
+			else
+				System.out.printf("The length starting at %d is NOT long\n", value);
+		}
+
+		System.out.println();
+
+		double proportion = Hailstone.propLong(10);
+		System.out.printf("The proportion of sequences from 1 to 10 that are long is %3.2f\n", proportion);
+
+		proportion = Hailstone.propLong(100);
+		System.out.printf("The proportion of sequences from 1 to 100 that are long is %3.2f\n", proportion);
+
+	}
+
 }
